f6c93234f410f6c1a8442fe923428204cfec43a3
usuario <alu0101392561@ull.edu.es>
2024-12-07 11:32:12 +0000

    Beginning of the the redisign of the question page user

--- src/src/page_components/QuestionPageUser.js
+++ src/src/page_components/QuestionPageUser.js
@@ -40,54 +40,25 @@ export const QuestionPageUser = () => {
     navigate('/'); 
   };
 
-  const sendTextToServer = async () => {
-    if (!code.trim()) {
-      alert('No se introdujo ningún código. Por favor rellene la caja de entrada con su código');
-      return;
-    }
-  
-    try {
-      const response = await fetch('http://127.0.0.1:5000/save-text', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text: code }),
-      });
-  
-      const data = await response.json();
-  
-      if (response.ok) {
-        alert(data.message); 
-      } else {
-        alert(`Error: ${data.message}`);
-      }
-    } catch (error) {
-      alert('Error al conectar con el servidor.');
-      console.error(error);
-    }
-  };
   
   var tittle = "Titulo"
+  var description = "bla bla"
 
   return (
     <div className="App2">
       <div className="title-box">
         <h1>{tittle}</h1>
       </div>
-        <textarea className="code_input"
-          value={code}
-          onChange={(e) => saveCode(e.target.value)}
-          placeholder="Introduce tu código"
-        ></textarea>
+      <div className="description">
+        <h2>{description}</h2>
+      </div>
+        
       <div className="button-container-first-line">
         <input type="file" onChange={(e) => setFile(e.target.files[0])} className="file-input"/>
         <button className="button_submit" onClick={UploadFile}>
           Subir archivo
         </button>
-        <button className="button_submit" onClick={sendTextToServer}>
-          Enviar Código
-        </button>
+        
       </div>
       <div className="button-container-second-line" onClick={GoToUser}>
         <button className="button_submit">
