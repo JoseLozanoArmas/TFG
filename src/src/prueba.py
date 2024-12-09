ae2268ab02426f0177b74c52f42f9933e8d7d7c3
usuario <alu0101392561@ull.edu.es>
2024-12-09 18:53:56 +0000

    New files to probe added

--- src/src/prueba.py
+++ src/src/prueba.py
@@ -1,14 +1,22 @@
 import os
+import re
 
-# Ruta del archivo
-file_path = "../files_to_probe/probando_posibles_fallos.py"
+def ejecute_code(route):
+    si = r".*\.(py|cc?|rb)"
+    # Comprobamos que el archivo existe
+    print(route)
+    if os.path.exists(route):
+        # Si existe lo leemos
+        with open(route, "r") as file:
+            # Guardamos y ejecutamos lo leído
+            code = file.read()
+            exec(code)
+    else:
+        print(f"El archivo en la ruta '{route}' no existe.")
 
-# Comprobamos que el archivo existe
-if os.path.exists(file_path):
-    # Si existe lo leemos
-    with open(file_path, "r") as file:
-        # Guardamos y ejecutamos lo leido
-        code = file.read()
-        exec(code)
-else:
-    print(f"El archivo en la ruta '{file_path}' no existe.")
+file_path_python = "../files_to_probe/print_hello_world.py"
+file_path_c_plus = "../files_to_probe/print_hello_world.cc"
+file_path_c = "../files_to_probe/print_hello_world.c"
+file_path_ruby = "../files_to_probe/print_hello_world.rb"
+file_path_ejecutable = "../files_to_probe/a.out"
+ejecute_code(file_path_ejecutable)
