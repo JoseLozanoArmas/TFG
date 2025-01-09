68f24dbc90aa8c97385f33bfad3d58c8dd09f00e
JoseLozanoArmas <alu0101392561@ull.edu.es>
2025-01-09 18:57:00 +0000

    Difference between admin and not admin added

--- src/page_components/ControlPanel.js
+++ src/page_components/ControlPanel.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ControlPanel.css';
 
 export const ControlPanel = () => {
   const navigate = useNavigate();
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const userRole = localStorage.getItem('user_role');
+    setIsAdmin(userRole === 'admin');
+  }, []);
 
   const MoveToBlockGeneralAdminPage = () => {
     navigate('/block_general_admin_page');
@@ -17,28 +23,50 @@ export const ControlPanel = () => {
     navigate('/settings')
   };
 
-  return (
-    <div className="control_panel_page">
-      <div className="button_container">
-        <span className="button_label">Ranking</span>
-        <button className="button_control_panel" onClick={MoveToRanking}>
-          <img src={require("../img/prize.png")} className="button_icon" alt="Ranking" />
-        </button>
+  if (isAdmin) {
+    return (
+      <div className="control_panel_page">
+        <div className="button_container">
+          <span className="button_label">Ranking</span>
+          <button className="button_control_panel" onClick={MoveToRanking}>
+            <img src={require("../img/prize.png")} className="button_icon" alt="Ranking" />
+          </button>
+        </div>
+  
+        <div className="button_container">
+          <span className="button_label">Configuración</span>
+          <button className="button_control_panel" onClick={MoveToSettings}>
+            <img src={require("../img/configuracion.png")} className="button_icon" alt="Configuración" />
+          </button>
+        </div>
+  
+        <div className="button_container">
+          <span className="button_label">Bloque de preguntas</span>
+          <button className="button_control_panel" onClick={MoveToBlockGeneralAdminPage}>
+            <img src={require("../img/prueba.png")} className="button_icon" alt="Preguntas" />
+          </button>
+        </div>
       </div>
+    );
+  } else {
+    return (
+      <div className="control_panel_page">
+        <div className="button_container">
+          <span className="button_label">Ranking</span>
+          <button className="button_control_panel" onClick={MoveToRanking}>
+            <img src={require("../img/prize.png")} className="button_icon" alt="Ranking" />
+          </button>
+        </div>
+  
+        <div className="button_container">
+          <span className="button_label">Bloque de preguntas</span>
+          <button className="button_control_panel" onClick={MoveToBlockGeneralAdminPage}>
+            <img src={require("../img/prueba.png")} className="button_icon" alt="Preguntas" />
+          </button>
+        </div>
+      </div>
+    );
+  }
 
-      <div className="button_container">
-        <span className="button_label">Configuración</span>
-        <button className="button_control_panel" onClick={MoveToSettings}>
-          <img src={require("../img/configuracion.png")} className="button_icon" alt="Configuración" />
-        </button>
-      </div>
-
-      <div className="button_container">
-        <span className="button_label">Bloque de preguntas</span>
-        <button className="button_control_panel" onClick={MoveToBlockGeneralAdminPage}>
-          <img src={require("../img/prueba.png")} className="button_icon" alt="Preguntas" />
-        </button>
-      </div>
-    </div>
-  );
+  
 };
