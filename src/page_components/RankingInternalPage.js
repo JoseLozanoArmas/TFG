8c66d1157fb0aba00c17d6e9c11b0ad48a47d525
JoseLozanoArmas <alu0101392561@ull.edu.es>
2025-02-15 21:05:08 +0000

    Progress in the internal rankings

--- src/page_components/RankingInternalPage.js
+++ src/page_components/RankingInternalPage.js
@@ -8,7 +8,7 @@ export const RankingInternalPage = () => {
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const [saveJson, setSaveJson] = useState();
+  const [saveJson, setSaveJson] = useState(null);
   const block_id = "block_1"
   const block_number = 1
   
@@ -21,54 +21,86 @@ export const RankingInternalPage = () => {
     }
   });
 
-  const getJsonData = async() => {
-    try { 
-      const response = await fetch(route_to_server + 'get-rankings-info', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 
-          text: block_id 
-        }),
-      });
-      const data = await response.json();
-      if (response.ok) {
-        setSaveJson(JSON.parse(data.data))
-      } else {
-        alert(`Error: ${data.message}`);
-      }
-    } catch (error) {
-      alert('Error al conectar con el servidor.');
-      console.error(error);
-    }
-  }
-
-  const getUserInformation = () => {
-    for (let i = 0; i < saveJson.length; ++i) {
-      alert(saveJson[i].username)
-    }
-  }
-
   useEffect(() => {
+    const getJsonData = async() => {
+      try { 
+        const response = await fetch(route_to_server + 'get-rankings-info', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 
+            text: block_id 
+          }),
+        });
+        const data = await response.json();
+        if (response.ok) {
+          setSaveJson(JSON.parse(data.data));
+        } else {
+          alert(`Error: ${data.message}`);
+        }
+      } catch (error) {
+        alert('Error al conectar con el servidor.');
+        console.error(error);
+      }
+    }
     getJsonData();
   }, []);
 
   useEffect(() => {
+    if (!saveJson) { return; }
+    const getUserInformation = () => {
+      let save_slots = []
+      for (let i = 0; i < saveJson.length; ++i) {
+        saveJson[i]["id"] = i + 1;
+        save_slots.push(saveJson[i])
+      }
+      setUserSlots(save_slots)
+    }
+    getUserInformation();
+  }, [saveJson])
+
+  useEffect(() => {
     localStorage.setItem(`user_slots_${id}`, JSON.stringify(userSlots))
   })
-  
 
-  getUserInformation()
+  
 
   return (
     <div className="App_ranking_internal_page">
       <div className="tittle_ranking_internal_page">
         <h1>Ranking de puntuaciones del bloque {block_number}</h1>
       </div>
-      <div>
-        
+      <div className="user_slot">
+          <div className="slot_id">
+            Posición
+          </div>
+          <div className="slot_username">
+            Usuario
+          </div>
+          <div className="slot_puntuation">
+            Puntuación
+          </div>
+          <div className="slot_time">
+            Tiempo total
+          </div>
       </div>
+      {userSlots.map((slot) => (
+        <div key={slot.id} className="user_slot">
+          <div className="slot_id">
+            {slot.id}
+          </div>
+          <div className="slot_username">
+            {slot.username}
+          </div>
+          <div className="slot_puntuation">
+            {slot.puntuation}
+          </div>
+          <div className="slot_time">
+            {slot.time}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
